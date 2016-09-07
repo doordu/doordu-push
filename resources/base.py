124a27f5b4ff0b5593a63c12586fcb96444ba854
aidan <erhuabushuo@gmail.com>
2016-09-07 16:16:01 +0800

    日志信息增加行号

--- resources/base.py
+++ resources/base.py
@@ -5,7 +5,7 @@ class Base:
     def __init__(self, config):
         self.logger = logging.getLogger('doordu-push')
         fh = logging.FileHandler('doordu-push.log')
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(lineno)d - %(message)s')
         fh.setLevel(logging.INFO)
         fh.setFormatter(formatter)
         self.logger.addHandler(fh)
