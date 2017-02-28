6ee59daacc1ebf8cb72e258b588011873a0fd9f3
aidan <erhuabushuo@gmail.com>
2017-02-28 18:37:13 +0800

    修正日志重复问题

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+import logging
 
 import falcon
 import yaml
+from raven import Client
 
 from resources.push import PushResource
 from resources.online import OnlineResource
@@ -14,8 +16,17 @@ config = None
 with open(config_file, "r") as handle:
     config = yaml.load(handle)
 
-push = PushResource(config)
-online = OnlineResource(config)
+logger = logging.getLogger('doordu-push')
+fh = logging.FileHandler(os.path.join(os.path.dirname(__file__), 'doordu-push.log'))
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(lineno)d - %(message)s')
+fh.setLevel(logging.INFO)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+logger.setLevel(logging.INFO)
+client = Client('https://fa6faa5cea834cbd9cfc404fb72de7aa:6b9ac91556d8405da3bf1f75e20e4239@sentry.io/104967')
+
+push = PushResource(config, logger, client)
+online = OnlineResource(config, logger, client)
 
 app.add_route('/push', push)
 app.add_route('/online', online)
--- resources/base.py
+++ resources/base.py
@@ -1,20 +1,13 @@
 import logging
 import os
 
-from raven import Client
 
 
 class Base:
-    def __init__(self, config):
-        self.logger = logging.getLogger('doordu-push')
-        fh = logging.FileHandler(os.path.join(os.path.dirname(__file__), 'doordu-push.log'))
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(lineno)d - %(message)s')
-        fh.setLevel(logging.INFO)
-        fh.setFormatter(formatter)
-        self.logger.addHandler(fh)
-        self.logger.setLevel(logging.INFO)
+    def __init__(self, config, logger, client):
         self.config = config
-        self.client = Client('https://fa6faa5cea834cbd9cfc404fb72de7aa:6b9ac91556d8405da3bf1f75e20e4239@sentry.io/104967')
+        self.logger = logger
+        self.client = client
 
         setup = getattr(self, 'setup')
         if setup:
